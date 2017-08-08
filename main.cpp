13fb1eaff9942ffcf68ca6a0aadf042c4607c2d4
Вячеслав <fram5109@outlook.com>
2017-08-08 14:44:25 +0300

    готово

--- main.cpp
+++ main.cpp
@@ -8,12 +8,9 @@ int main()
 	wavefront_model eye_inner("models\\african_head\\african_head_eye_inner.obj");
 	wavefront_model floor("models\\floor\\floor.obj");
 
-	renderer::set_viewport(1200, 1200);
-	renderer::set_light(vec3({ 3.0f, -1.0f, -2.0f }));
-	renderer::set_view(vec3({ 0.0f, 0.0f, 0.0f }), vec3({ 0.0f, 0.0f, 5.0f }), vec3({ 0.0f, 1.0f, 0.0f }));
-
-	head.scale(vec3{ 0.8f, 0.8f, 0.8f });
-	eye_inner.scale(vec3{ 0.8f, 0.8f, 0.8f });
+	renderer::set_viewport(800, 800);
+	renderer::set_light(vec3({ -3.0f, -1.0f, -2.0f }));
+	renderer::set_view(vec3({ 0.0f, 0.0f, 0.0f }), vec3({ 1.0f, 1.0f, 5.0f }), vec3({ 0.0f, 1.0f, 0.0f }));
 
 	std::vector<std::reference_wrapper<wavefront_model>> models;
 	models.push_back(std::ref(eye_inner));
